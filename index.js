6b545f6efa043d35422b60db99605107ace01360
Manju KC <Manju KC>
2019-05-27 22:56:28 -0400

    Updated all api message

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const server = express();
 const db = require('./data/db')
 
 server.post("/api/users" , (req, res) => {
-    res.send("Good Buye World")
+    const param = req.body;
+    console.log(param)
+    db.insert(param).then((result) => {
+       res.json({ result });
+    })
 })
 
 server.get("/api/users" , (req, res) => {
@@ -13,6 +17,30 @@ server.get("/api/users" , (req, res) => {
         res.json({ data });
     }).catch(() => {
         res.statusCode(500).send({ error: "The users information could not be retrieved." })
+    })
+})
+
+server.get("/api/users/:id" , (req, res) => {
+    db.findById(req.params.id).then((data) => {
+        res.json({ data });
+    }).catch(() => {
+        res.statusCode(500).send({ error: "The user information could not be retrieved." })
+    })
+})
+
+server.delete("/api/users/:id" , (req, res) => {
+    db.remove(req.params.id).then((data) => {
+        res.json({ data });
+    }).catch(() => {
+        res.statusCode(500).send({ error: "The user could not be removed" })
+    })
+})
+
+server.put("/api/users/:id" , (req, res) => {
+    db.update(req.params.id, req.body).then((data) => {
+        res.json({ data });
+    }).catch(() => {
+        res.statusCode(500).send({ error: "The user information could not be modified." })
     })
 })
 
